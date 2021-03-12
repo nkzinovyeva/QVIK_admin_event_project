b7717c9ccef92dd6f0a35cc7c05d90a5a78f096f
Piranavan Thambirajah <piranavanpth@gmail.com>
2021-03-12 14:52:34 +0200

    Added fields on event page

--- src/components/Events.js
+++ src/components/Events.js
@@ -1,11 +1,24 @@
 import React from "react";
+import FormControl from '@material-ui/core/FormControl';
+
+import TextField from '@material-ui/core/TextField';
+
 
 function Events() {
 
       return(
         <div style={{marginLeft: '150px'}}>
-           <h1>Events page</h1>
+          <h1>Events page</h1>
+          <form noValidate autoComplete="off">
+            <div>
+              <TextField helperText="Event name" id="standard-basic" label="Nights of Arts" />
+            </div>
+            <div>
+              <TextField helperText="Location" id="standard-basic" label="Helsinki" />
+            </div>
+          </form>
         </div>
+        
       );
     }
 
