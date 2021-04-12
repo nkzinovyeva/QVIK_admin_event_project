4993bf7c8e7768ed28659b91c5cad21f5c247411
ColineFardel <coline.fardel@gmail.com>
2021-04-12 15:19:26 +0300

    Function to get events' names - Coline

--- src/AppMenu.js
+++ src/AppMenu.js
@@ -1,18 +1,22 @@
-import React, {useEffect, useState} from "react";import { makeStyles, createStyles } from '@material-ui/core/styles'
+import React, { useEffect, useState } from "react"; import { makeStyles, createStyles } from '@material-ui/core/styles'
 
 import List from '@material-ui/core/List'
 
 import AppMenuItem from './AppMenuItem'
 
-const appMenuItems = [
-  {
-    name: 'Home',
-    link: '/',
-  },
-  {
-    name: 'Restaurants',
-    link: '/restaurants',
-    items: [
+const AppMenu = () => {
+  const classes = useStyles()
+  const [events, setEvents] = useState([]);
+
+  const appMenuItems = [
+    {
+      name: 'Home',
+      link: '/',
+    },
+    {
+      name: 'Restaurants',
+      link: '/restaurants',
+      items: [
         {
           name: 'Restaurant 1',
         },
@@ -22,50 +26,38 @@ const appMenuItems = [
         {
           name: 'Restaurant 3',
         },
-    ]
-  },
-  {
-    name: 'Events',
-    link: '/events',
-    items: [ //HERE WE NEED TO WRITE A FUNCTION WHERE EACH NAME = EVENTS.TITLE
-        {
-          name: 'Event 1',
-        },
-        {
-          name: 'Event 2',
-        },
-        {
-          name: 'Event 3',
-        },
-    ]
-  },
-]
+      ]
+    },
+    {
+      name: 'Events',
+      link: '/events',
+      items: events.map(event => {
+        return { name: event.title }
+      })
+    },
+  ]
 
-const AppMenu = () => {
-  const classes = useStyles()
-  const [events, setEvents] = useState([]);
-  
-    useEffect(() => {
-        getEvents(); 
-    }, []);
-    
-    const getEvents = () => {
-        fetch("https://qvik.herokuapp.com/api/v1/events")
-          .then((response) => response.json())
-          .then((jsondata) => { 
-            console.log('jsondata', jsondata.data[0].data )
-            setEvents(jsondata.data[0].data);
-          })
-            .catch(err => console.error(err));
-    };
+  useEffect(() => {
+    getEvents();
+  }, []);
+
+  const getEvents = () => {
+    fetch("https://qvik.herokuapp.com/api/v1/events")
+      .then((response) => response.json())
+      .then((jsondata) => {
+        console.log('jsondata', jsondata.data[0].data)
+        setEvents(jsondata.data[0].data);
+      })
+      .catch(err => console.error(err));
+  };
 
   return (
-        <List component="nav" className={classes.appMenu} disablePadding>
-          {appMenuItems.map((item, index) => (
-            <AppMenuItem {...item} key={index} />
-          ))}
-        </List>
-      )
+    <List component="nav" className={classes.appMenu} disablePadding>
+      {appMenuItems.map((item, index) => (
+        <AppMenuItem {...item} key={index} />
+      ))}
+    </List>
+  )
 }
 
 const drawerWidth = 240
